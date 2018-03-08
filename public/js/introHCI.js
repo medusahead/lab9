23a8669485aa311e4040b859147bcc0b90966d91
joj.m <jojmorataya@gmail.com>
2018-03-07 18:28:49 -0800

    YELLOW bugfix -_-

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -1,4 +1,4 @@
-'use strict';
+//'use strict';
 
 // Call this function when the page loads (the "ready" event)
 $(document).ready(function() {
@@ -12,14 +12,14 @@ function initializePage() {
 
 // Function that is called when the server responds to the AJAX call
 function onServerResponse(data){
-    var labElementId = getLabElementId(data.id);
-    var labElement = $(labElementId);
+    var hehe = getLabElementId(data.id);
+    var labElement = $(hehe);
     labElement.css({'background': 'yellow'});
 }
 
 
 // Function to format the CSS selector
 function getLabElementId(id){
-    var labElementId = '#lab_' + id;
+    var labElementId = '#lab-' + id;
     return labElementId;
 }
